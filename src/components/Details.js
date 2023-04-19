c505493f922bc04332249c78aebca0b6ac6cada8
Kaif <kaifshaikh2013.sk@gmail.com>
2023-04-20 03:00:15 +0530

    Made Some Api Calls from the GitHub Api

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,24 @@
+import Typed from "react-typed";
+
 const Details = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
     <div>
-      <p>{props.text}</p>
+      {props.status ? (
+        <div>
+          <img className="rounded-full" src={props.data.avatar_url} />
+        </div>
+      ) : (
+        <p className="text-2xl font-bold text-white min-w-[80%]  text-center py-24 ">
+          <Typed
+            className="text-2xl"
+            strings={["Type some profile-names in the textbox to get Profiles"]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+        </p>
+      )}
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Details from "./Details";
 const Search = () => {
   const [searchText, setSearchText] = useState("");
+  const [apiFetch, setApiFetch] = useState(false);
+  const [data, setData] = useState(null);
+
+  async function getApi() {
+    let data = await fetch("https://api.github.com/users/" + searchText);
+    const json = await data.json();
+    //console.log(json);
+    setData(json);
+
+    setApiFetch(true);
+  }
+
   return (
     <div>
       <input
         type="text"
+        className="text-black w-5/6 m-8 rounded p-2 text-2xl font-bold"
         value={searchText}
         onChange={(e) => {
           setSearchText(e.target.value);
-          console.log(searchText);
+          //console.log(searchText);
         }}
       />
-      <button>Search</button>
-      <Details text={searchText} />
+      <button
+        className="hover:border-b-2 hover:border-blue-400 text-white transition-all font-bold text-2xl ease-in"
+        onClick={() => getApi()}
+      >
+        Search
+      </button>
+      <Details text={searchText} status={apiFetch} data={data} />
     </div>
   );
 };
