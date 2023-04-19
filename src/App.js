dc618df0191ebfa0b0c84e7c92c740026c4faca1
Kaif <kaifshaikh2013.sk@gmail.com>
2023-04-19 20:44:15 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+  return <div className="text-2xl ">Hello</div>;
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
